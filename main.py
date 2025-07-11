730398b321feec82df28f030f13d628d0364595b
Higor <hfashigor@gmail.com>
2025-07-11 16:30:46 -0300

    Making adjusts to command line

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Main script to analise Cryptos")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    parser.add_argument("--crypto", type=str, required=False, help="Cryptocurrency symbol (e.g., AAVEBTC, DOGEBTC)")
-    parser.add_argument("--model", type=str, required=False, help="Model")
+    # Check the effects of this command later
+    # subparsers.required = True
+
+    predict = subparsers.add_parser("predict", help="Use a model do predict a coin")
+    predict.add_argument("--crypto", type=str, required=True, help="Cryptocurrency symbol (e.g., AAVEBTC, DOGEBTC)")
+    predict.add_argument("--model", type=str, required=True, help="Model")
+    predict.add_argument("--figure", type=str, default="out.png", required=False, help="Figure file out")
+    predict.add_argument("--excel", type=str, default=None, required=False, help="Excel file out")
+    predict.set_defaults(func=predict_exec)
+    
     parser.add_argument('--list', nargs='+', help='List: crypto, model')
-
-    parser.add_argument("--figure", type=str, default="out.png", required=False, help="Figure file out")
-    parser.add_argument("--excel", type=str, default=None, required=False, help="Excel file out")
-
+    parser.set_defaults(func=main_exec)
 
     args = parser.parse_args()
     return args
@@ -30,22 +36,30 @@ def parse_args():
 # for coin, folds in folds_por_moeda.items():
 #     analisar_moeda(coin, folds)
 
-def list_coins(list: list[str]):
-    if 'crypto' in list:
-        print("-"*30)
-        print("Coins:")
-        print("-"*30)
-        for c in coins: print(c)
-        print("-"*30, "\n")
+def main_exec(args):
+    if args.list:
+        list = args.list
+        if 'crypto' in list:
+            print("-"*30)
+            print("Coins:")
+            print("-"*30)
+            for c in coins: print(c)
+            print("-"*30, "\n")
 
-    if 'model' in list:
-        print("-"*30)
-        print("Models:")
-        print("-"*30)
-        print("MLP_REGRESSOR")
+        if 'model' in list:
+            print("-"*30)
+            print("Models:")
+            print("-"*30)
+            print("MLP_REGRESSOR")
 
 
-def crypto_model(crypto: str, model: str, figure: str, excel: str | None):
+def predict_exec(args):
+    crypto: str = args.crypto
+    model: str = args.model
+    figure: str = args.figure
+    excel: str | None = args.excel
+    
+
     logging.info(f"Starting {model} for crypto {crypto}")
     logging.warning(f"Other models aren't implemented yet")
     df = load(crypto)
@@ -89,14 +103,6 @@ def crypto_model(crypto: str, model: str, figure: str, excel: str | None):
 
 if __name__ == '__main__':
     args = parse_args()
-
-    if args.list: list_coins(args.list)
     
-    if args.crypto or args.model:
-        if not args.crypto:
-            logging.error("Incorrect using of model without crypto")
-            exit(1)
-        if not args.model:
-            logging.error("Incorrect using of crypto without model")
-            exit(1)
-        crypto_model(args.crypto, args.model, args.figure, args.excel)
+    if hasattr(args, 'func'):
+        args.func(args)
